0f28db64498f36979edbbe6d6e7fbb25ed777eaa
tskull01 <tskulley29@yahoo.com>
2020-07-19 13:14:18 -0400

    serving react app

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const cors = require("cors");
+const path = require("path");
 
 app.use(cors());
 //app.use for use with middleware passes data through a function
@@ -11,11 +12,15 @@ app.use((req, res, next) => {
   console.log("have to call next");
   next();
 });
-app.get("/", (req, res) => res.send("req.body"));
-//routes can be regex or exact matching + parameter matching
-app.post("/", (req, res) => {
-  res.sendDate = true;
+app.use(express.static(path.join(__dirname, "./fs-react/build")));
+app.put("/spotToPut", (req, res) => {
+  console.log("putting");
 });
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, +"./fs-react/build/index.html"))
+);
+//routes can be regex or exact matching + parameter matching
+app.post("/", (req, res) => {});
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
