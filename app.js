16b1706543bf2271a708a6eb3ec19c194b1c52ff
tskull01 <tskulley29@yahoo.com>
2020-07-18 23:49:02 -0400

    reps

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const app = express();
 const port = 3000;
 const cors = require("cors");
 
+app.use(cors());
+//app.use for use with middleware passes data through a function
 app.get("/", (req, res) => res.send(req.body));
-app.put("/", (req, res) => req.body)
+app.put("/", (req, res) => {
+  res.sendDate;
+});
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
