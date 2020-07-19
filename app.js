8a977a52937cca23a838693d16c4fd78d00b7f79
Tyler <skulley.tyler@gmail.com>
2020-07-19 19:41:20 +0000

    :cry:

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ const path = require("path");
 //app.use for use with middleware passes data through a function
 //example middleware
 app.put("/", (req, res) => {
-  console.log("putting");
+  res.send({"dirname" : __dirname});
 });
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "fs-react","build/index.html"))
-);
+app.use(express.static(path.join(__dirname, "fs-react/build/static")))
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "fs-react/build", "index.html"));
+});
 //routes can be regex or exact matching + parameter matching
 app.post("/", (req, res) => {res.send({"message": "working"})});
 app.listen(port, () =>
