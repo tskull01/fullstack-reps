1fe1a1cf0f800ce58c922baaf09a9eba16f9770f
Tyler <skulley.tyler@gmail.com>
2020-07-19 03:44:53 +0000

    installed cors

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const cors = require("cors");
 
-app.get("/", (req, res) => express.static("./fs-react/build"));
-
+app.get("/", (req, res) => res.send(req.body));
+app.put("/", (req, res) => req.body)
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
