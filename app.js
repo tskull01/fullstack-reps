c2f51bfa75b3492f8dc4ad804a8179835847cce5
Tyler <skulley.tyler@gmail.com>
2020-07-19 18:20:57 +0000

    hmm

--- app.js
+++ app.js
@@ -4,23 +4,16 @@ const port = 3000;
 const cors = require("cors");
 const path = require("path");
 
-app.use(cors());
 //app.use for use with middleware passes data through a function
 //example middleware
-app.use((req, res, next) => {
-  console.log("Can do whatever");
-  console.log("have to call next");
-  next();
-});
-app.use(express.static(path.join(__dirname, "./fs-react/build")));
-app.put("/spotToPut", (req, res) => {
+app.put("/", (req, res) => {
   console.log("putting");
 });
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, +"./fs-react/build/index.html"))
+  res.sendFile(path.join(__dirname, "fs-react","build/index.html"))
 );
 //routes can be regex or exact matching + parameter matching
-app.post("/", (req, res) => {});
+app.post("/", (req, res) => {res.send({"message": "working"})});
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
