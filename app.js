8fa970904eaab65ed5514560559983384514a3a5
tskull01 <tskulley29@yahoo.com>
2020-07-20 00:35:05 -0400

    :rain:

--- app.js
+++ app.js
@@ -3,18 +3,32 @@ const app = express();
 const port = 3000;
 const cors = require("cors");
 const path = require("path");
+const mongoose = require("mongoose");
 
 //app.use for use with middleware passes data through a function
 //example middleware
-app.put("/", (req, res) => {
-  res.send({"dirname" : __dirname});
-});
-app.use(express.static(path.join(__dirname, "fs-react/build/static")))
+
+app.use(express.static(path.join(__dirname, "fs-react/build")));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "fs-react/build", "index.html"));
 });
 //routes can be regex or exact matching + parameter matching
-app.post("/", (req, res) => {res.send({"message": "working"})});
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
+app.post("/", (req, res) => {
+  res.send({ message: "working" });
+});
+app.put("/", (req, res) => {
+  res.send({ dirname: __dirname });
+});
+const listenFunction = async () => {
+  let connection = await mongoose.connect(
+    "mongodb://fullstack.cyou:27017/bjj",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+  console.log(connection);
+  console.log(`Example app listening at http://localhost:${port}`);
+};
+app.listen(port, listenFunction);
