da789f1d5f8b449d1cb6848a62711d840c5a7420
tskull01 <tskulley29@yahoo.com>
2020-07-27 20:44:16 -0400

    :cry:

--- app.js
+++ app.js
@@ -3,19 +3,11 @@ const app = express();
 const port = 3000;
 const path = require("path");
 const mongoose = require("mongoose");
-const tunnel = require("tunnel-ssh");
 const dotenv = require("dotenv");
 dotenv.config();
 //app.use for use with middleware passes data through a function
 //example middleware
 
-let config = {
-  agent: "tyler",
-  host: "142.93.112.45",
-  port: 22,
-  privateKey: process.env.fullstack_key,
-  dstPort: 27017,
-};
 app.use(express.static(path.join(__dirname, "fs-react/build")));
 
 app.get("/", (req, res) => {
@@ -28,20 +20,17 @@ app.post("/", (req, res) => {
 app.put("/", (req, res) => {
   res.send({ dirname: __dirname });
 });
-let server = tunnel(config, (error, server) => {
-  if (error) {
-    console.log("SSH failed");
-  }
-  mongoose
-    .connect("mongodb://fullstack.cyou:27017/bjj", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then((connect) => {
-      app.listen(port, () => {
-        console.log(
-          `App listening on port ${port}` + `database connection ${connect}`
-        );
-      });
+
+mongoose
+  .connect("mongodb://fullstack.cyou:27017/bjj", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((connect) => {
+    app.listen(port, () => {
+      console.log(
+        `App listening on port ${port}` + `database connection ${connect}`
+      );
     });
-});
+  })
+  .catch((e) => console.log(`Invalid connection ${e}`));
