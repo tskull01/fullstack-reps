0f409e83f5c203338ac3d0a536247b07efc5dffd
Tyler <skulley.tyler@gmail.com>
2020-07-30 03:10:54 +0000

    ang served

--- app.js
+++ app.js
@@ -4,39 +4,39 @@ const port = 3000;
 const path = require("path");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const teamRouter = require("./resources/team/team.router");
-const scheduleRouter = require("./resources/schedule/schedule.router");
-const meetRouter = require("./resources/meet/meet.router");
-const matchRouter = require("./resources/match/match.router");
-const locationRouter = require("./resources/location/location.router");
-const competitorRouter = require("./resources/competitor/competitor.router ");
+//const teamRouter = require("./resources/team/team.router");
+//const scheduleRouter = require("./resources/schedule/schedule.router");
+//const meetRouter = require("./resources/meet/meet.router");
+//const matchRouter = require("./resources/match/match.router");
+//const locationRouter = require("./resources/location/location.router");
+//const competitorRouter = require("./resources/competitor/competitor.router ");
 
 dotenv.config();
 //app.use for use with middleware passes data through a function
 //example middleware
 
 //app.use(express.static(path.join(__dirname, "fs-react/build")));
-/* 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "fs-react/build", "index.html"));
-});
-*/
-app.use(express.static(path.join(__dirname, "bjj-front/dist/bjj-front")));
+ 
+//app.get("/", (req, res) => {
+//  res.sendFile(path.join(__dirname, "fs-react/build", "index.html"));
+//});
+
+app.use(express.static(path.join(__dirname, "bjj-front/dist")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "bjj-front/dist/bjj-front", "index.html"));
+  res.sendFile(path.join(__dirname, "bjj-front/dist", "index.html"));
 });
 //routes can be regex or exact matching + parameter matching
 
 //api/league
 //api/schedule
 //
-app.use("/api/team", teamRouter);
-app.use("/api/schedule", scheduleRouter);
-app.use("/api/meet", meetRouter);
-app.use("/api/match", matchRouter);
-app.use("/api/location", locationRouter);
-app.use("/api/competitor", competitorRouter);
+//app.use("/api/team", teamRouter);
+//app.use("/api/schedule", scheduleRouter);
+//app.use("/api/meet", meetRouter);
+//app.use("/api/match", matchRouter);
+//app.use("/api/location", locationRouter);
+//app.use("/api/competitor", competitorRouter);
 
 mongoose
   .connect("mongodb://tskulley:password@fullstack.cyou:27017/bjj", {
